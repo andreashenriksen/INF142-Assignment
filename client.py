73f0ad8c8ce861520072b5ae6883aabd317091e2
andreashenriksen <andreassoland.99@gmail.com>
2023-02-23 14:25:57 +0100

    changed variables self.host and self.port to be private

--- client.py
+++ client.py
@@ -4,18 +4,19 @@ import socket as sock
 class Client:
 
     def __init__(self, host, port) -> None:
-        self.host = host
-        self.port = port
+        self._host = host
+        self._port = port
         self._role = ""
-        self.client_socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        self._client_socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 
     def connect_to_server(self):
-        self.client_socket.connect((self.host, self.port))
-        server_message = self.client_socket.recv(1024).decode()
+        self._client_socket.connect((self._host, self._port))
+        server_message = self._client_socket.recv(1024).decode()
         print(f"Assigned role \"{server_message}\" by server")
 
 
 if __name__ == "__main__":
     SERVER_HOST, SERVER_PORT = "localhost", 5000
+
     client = Client(SERVER_HOST, SERVER_PORT)
     client.connect_to_server()
